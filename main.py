1c5d6259a6d76c354039ede70c35c0eff6fe021a
Carlos Natanael <141105332+Srleo12@users.noreply.github.com>
2024-08-24 04:21:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from database import *
+import time
+
 
 def registrar_usuario():
-    print("\n== Registrar ==")
-    username = input("Digite o nome do usuário: ")
+    print("=== Registro ===")
+    username = input("Digite o nome de usuário: ")
     password = input("Digite a senha: ")
     role = input("Digite o papel do usuário (admin/user): ")
 
@@ -10,66 +12,108 @@ def registrar_usuario():
         print("Papel inválido. Use 'admin' ou 'user'.")
         return
 
-    if role not in ['admin','user']:
-        print("Erro ao selecionar usuário. Use 'admin' ou 'user'.")
-        return
     try:
         create_user(username, password, role)
-        print("Usuário registrado com sucesso!")
+        print("\nUsuário registrado com sucesso!")
     except IntegrityError:
-        print("Erro: Nome de usuário já existe.")
+        print("\nErro: Nome de usuário já existe.")
 
 def login():
-    print("\n== Login ==")
-    username = input("Digite o nome do usuário: ")
+    print("\n=== Login ===")
+    username = input("Digite o nome de usuário: ")
     password = input("Digite a senha: ")
 
     user = autenticacao_user(username, password)
     if user:
-        print(f"Bem-vindo, {user.username}! Você é um {user.role}")
+        print(f"\nBem-vindo, {user.username}! Você é um {user.role}.")
         return user
     else:
-        print("Nome de usuário ou senha incorretos.")
+        print("\nNome de usuário ou senha incorretos.")
         return None
 
-if __name__ == "__main__":
+
+def menu_admin():
+    while True:
+        print("\n=== Menu Administrador ===")
+        print("1 - Cadastrar usuário")
+        print("2 - Listar usuários")
+        print("3 - Excluir usuário")
+        print("4 - Logout")
+        opcao = input("Escolha uma opção: ")        
+
+        if opcao == '1':
+            CadastrarUsuario()
+        elif opcao == '2':
+            usuarios = listar_usuarios()
+            if usuarios:
+                print("\n=== Lista de Usuários ===")
+                for usuario in usuarios:
+                     print(f"\nNome: {usuario.nome} | Idade: {usuario.idade} | Email: {usuario.email} | CPF: {usuario.CPF} | Contato: {usuario.contato}")
+            else:
+                print("\nNenhum usuáio cadastrado.")
+        elif opcao == '3':
+            excluir_usuario()
+        elif opcao == '4':
+            print("Saindo...")
+            time.sleep(2)
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+def menu_usuario():
+    while True:
+        print("\n=== Menu Usuário ===")
+        print("1 - Listar usuários")
+        print("2 - Logout")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == '1':
+            usuarios = listar_usuarios()
+            if usuarios:
+                print("\n=== Lista de Usuários ===")
+                for usuario in usuarios:
+                     print(f"\nNome: {usuario.nome} | CPF: {usuario.CPF} |")
+            else:
+                print("Nenhum usuáio cadastrado.")
+        elif opcao == '2':
+            print("Saindo...")
+            time.sleep(2)
+            break
+        else:
+            print("Opção inválida ou permissão insuficiente. Tente novamente.")
+
+def main():
     db.connect()
-    db.create_tables([User,Usuario])
+    db.create_tables([User, Usuario])
 
     user = None
     while True:
         if not user:
-            print("\nEscolha uma opção:")
+            print("\n=== Sistema de Gerenciamento de Usuários ===")
             print("1 - Registrar novo usuário")
-            print("2 - Login")            
+            print("2 - Login")
             print("3 - Sair")
-            opcao = int(input("Digite o número da opção desejada: "))
+            opcao = input("Escolha uma opção: ")
 
-            if opcao == 1:
+            if opcao == '1':
                 registrar_usuario()
-            elif opcao == 2:
+            elif opcao == '2':
                 user = login()
-            elif opcao == 3:
+                if user:
+                    if user.role == 'admin':
+                        menu_admin()
+                    else:
+                        menu_usuario()
+            elif opcao == '3':
+                print("Saindo...")
+                time.sleep(2)
                 break
             else:
                 print("Opção inválida. Tente novamente.")
         else:
-             print("\nEscolha uma opção:")
-             print("1 - Cadastrar usuário")
-             print("2 - Listar usuários")
-             print("3 - Excluir usuários")
-             print("4 - Logout")
-             opcao = int(input("Digite o número da opção desejada: "))
-
-             if opcao == 1 and user.role == 'admin':
-                 CadastrarUsuario()
-             elif opcao == 2:
-                 usuarios = listar_usuarios()
-                 for usuario in usuarios:
-                     print(f"\nNome: {usuario.nome} | Email: {usuario.email} | CPF: {usuario.CPF} | Contato: {usuario.contato}")
-             elif opcao == 3 and user.role == 'admin':
-                 excluir_usuario()
-             elif opcao == 4:
-                 user = None
-             else:
-                 print("Opção inválida ou permissão insuficiente. Tente novamente.")
+            if user.role == 'admin':
+                menu_admin()
+            else:
+                menu_usuario()
+if __name__=="__main__":
+    main()
